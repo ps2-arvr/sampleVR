e87cfedf976cc8e771f6fd7be609d65b8a80f1b9
ps2-arvr <progressive.system2@gmail.com>
2019-10-25 17:08:07 +0900

    webCamera test

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,67 +6,75 @@ class App {
 	init() {
 		//ライトの色、光源の方向設定
 		var light = new THREE.DirectionalLight(0xFFFFFF);
-		light.position.set(0, 0, 0);
+		light.position.set(0, 2, 0);
 		scene.add( light );
 		//
-		var ambientLight = new THREE.AmbientLight(0xFFFFFF);
+		var ambientLight = new THREE.AmbientLight(0xFFF888);
 		scene.add( ambientLight );
-		//
-		this.meshCube = new THREE.Mesh();
-		//オブジェクトを作成できる
+
+		//webCameraの映像を取り込む
 		
-		var geometryCube = new THREE.BoxGeometry(10,10,10);
-		var materialCube = new THREE.MeshLambertMaterial( { color: 0xf6cece } );
+		// ブラウザ間差異対応
+		navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+		// ユーザにメディアデバイス（カメラ、マイクなど）の使用をを尋ねる
+		navigator.getUserMedia(
+  		 // カメラを使用して、マイクを使用しない場合
+  		 { video: true, audio: false },
+ 
+   		// localMediaStream（カメラ映像のストリームデータが含まれる）が取得できた場合
+   		function( localMediaStream ) {
+   		   // ここにlocalMediaStreamを取得できたときの処理を記述
+   		},
+   		// localMediaStreamが取得できなかった場合
+  		 function( err ) {
+  		    console.log( err );
+  		 }
+		);
+		// ブラウザ間差異対応
+		window.URL = window.URL || window.webkitURL;
+		// videoエレメント作成
+		var video = document.createElement( 'video' );
+		// URLオブジェクトを作成して、videoエレメントのsrcに設定
+		video.src = window.URL.createObjectURL( localMediaStream );
+		// 動画の再生
+		video.play();
+		
+		
+		// videoエレメント作成
+		//var video = document.createElement( 'video' );
+		// テクスチャにする動画の指定
+		//video.src = './video/test.mp4';
+		// 動画をループ再生
+		//video.loop = true;
+		// 動画のロード＆再生
+		//video.load();
+		//video.play();
+		
+		//videoテクスチャの作成
+		var texture = new THREE.VideoTexture( video );
+		texture.magFilter = THREE.LinearFilter;
+		texture.minFilter = THREE.LinearFilter;
+		texture.format = THREE.RGBFormat;
+		
+		//オブジェクトを作成する
+		this.meshCube = new THREE.Mesh();
+		var geometryCube = new THREE.BoxGeometry(30,30,30);
+		var materialCube = new THREE.MeshBasicMaterial( { map: texture }  );
 		this.meshCube = new THREE.Mesh( geometryCube, materialCube );
 		this.meshCube.position.set(10, 0, 50);
 		this.scene.add( this.meshCube );
 		this.meshCube.name='loadTorus'
-		
-		
-		navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-   		getUserMedia: function(c) {
-   		  return new Promise(function(y, n) {
-     		  (navigator.mozGetUserMedia ||
-      		navigator.webkitGetUserMedia).call(navigator, c, y, n);
-    		 });
-   		}
-		} : null);
-		if ( !navigator.mediaDevices ) {
- 		 alert( "getUserMedia() not supported." );
- 		 return;
-		}
-		// Prefer camera resolution nearest to 1280x720.
-		var constraints = { audio:false, video:true };
-		navigator.mediaDevices.getUserMedia( constraints )
-		.then( function( stream ) {
- 		 var video = document.querySelector( 'video' );
- 		 video.srcObject = stream
- 		 video.onloadedmetadata = function(e) {
-  		 video.play();
- 		 };
-		})
-		.catch(function(err) {
- 		 alert( err.name + ": " + err.message );
-		});
 
-		var texture = new THREE.Texture( video );
-                texture.generateMipmaps = false;
-                texture.minFilter = THREE.NearestFilter;
-                texture.maxFilter = THREE.NearestFilter;
-                texture.format = THREE.RGBFormat;
-		
-		setInterval( function () {
-                  if ( video.readyState >= video.HAVE_CURRENT_DATA ) {
-                    texture.needsUpdate = true;
-                  }
-                 } ,1000 / 24 );
 
-		this.geometrySphere = new THREE.SphereGeometry(1000,32,32);
-		this.geometrySphere.scale(-1, 1, 1);
-		this.material = new THREE.MeshBasicMaterial( { map: texture } ) ;
-		this.meshSphere = new THREE.Mesh( this.geometrySphere, this.material);
-		this.meshSphere = new THREE.Mesh( geometrySphere, material );
-		this.meshSphere.position.set(0, 0, 0);
+		var meshSphere = new THREE.Mesh();
+		var loaderSphere = new THREE.TextureLoader();
+		//バックグラウンドの画像指定
+		var textureSphere = loaderSphere.load( './img/photo.jpg');
+		var materialSphere = new THREE.MeshBasicMaterial({ map:textureSphere, side:THREE.BackSide });
+		var geometrySphere = new THREE.SphereGeometry(1000,32,32);
+		meshSphere = new THREE.Mesh( geometrySphere, materialSphere );
+		meshSphere.position.set(0, 0, 0);
+		//
 		this.scene.add( meshSphere );
 	}
 
